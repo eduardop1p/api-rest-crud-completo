801b9011ccc72f4954ac801373f78f481d988a41
eduardop1p <el702985@gmail.com>
2022-03-28 16:32:04 -0300

    mudando porta sevidor

--- dist/sever.js
+++ dist/sever.js
@@ -1,8 +1,6 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 var _severConfig = require('./config/severConfig'); var _severConfig2 = _interopRequireDefault(_severConfig);
 
-const PORT = process.env.SEVER_PORT;
-
-_app2.default.on('go', () => _app2.default.listen(PORT || 3000, () => {
+_app2.default.on('go', () => _app2.default.listen(process.env.PORT || 3000, () => {
   console.log(`Sevidor rodando em ${_severConfig2.default.url}`);
 }));
--- src/sever.js
+++ src/sever.js
@@ -1,8 +1,6 @@
 import app from './app';
 import severConfig from './config/severConfig';
 
-const PORT = process.env.SEVER_PORT;
-
-app.on('go', () => app.listen(PORT || 3000, () => {
+app.on('go', () => app.listen(process.env.PORT || 3000, () => {
   console.log(`Sevidor rodando em ${severConfig.url}`);
 }));
