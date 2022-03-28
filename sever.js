96a4a6c8513d96a1ae4d2df8ba650f3c083f6ee0
eduardop1p <el702985@gmail.com>
2022-03-28 15:04:11 -0300

    update-sever.js

--- sever.js
+++ sever.js
@@ -1,6 +1,8 @@
 import app from './app';
 import severConfig from './config/severConfig';
 
-app.on('go', () => app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.SEVER_PORT;
+
+app.on('go', () => app.listen(PORT || 3000, () => {
   console.log(`Sevidor rodando em ${severConfig.url}`);
 }));
