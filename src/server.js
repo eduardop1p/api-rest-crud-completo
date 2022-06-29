7e3052422ecdf50f6ce3107efb40e5f7f794bfa9
eduardop1p <el702985@gmail.com>
2022-06-29 14:03:20 -0300

    atz server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app';
 import severConfig from './config/severConfig';
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 4050, () => {
   console.log(`Sevidor rodando em ${severConfig.url}`);
 });
