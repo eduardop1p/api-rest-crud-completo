10accaa5541404e50ed4827b0cb4053bc0b07ced
eduardop1p <el702985@gmail.com>
2022-06-29 22:10:18 -0300

    atz server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 import app from './app';
 
-console.log(process.env.SEVER_PORT);
-
 app.listen(process.env.SEVER_PORT, () => {
   console.log(`Sevidor rodando em http://localhost:${process.env.SEVER_PORT}`);
 });
