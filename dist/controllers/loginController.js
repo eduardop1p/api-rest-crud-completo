c46c205c04cc5de7bfd99d868db5e6baf6d2759a
eduardop1p <el702985@gmail.com>
2022-07-28 21:30:16 -0300

    atz controller login

--- dist/controllers/loginController.js
+++ dist/controllers/loginController.js
@@ -7,16 +7,24 @@ class LoginController {
     const user = await userLogin.onLogin();
 
     if (userLogin.errors.length > 0) {
-      res.json({ errors: userLogin.errors });
+      res.status(400).json({ errors: userLogin.errors });
       return;
     }
 
     req.session.user = user;
     req.session.save();
-
     const { _id, nome, email } = user;
+    const { cookie } = req.session;
 
-    res.json({ _id, nome, email });
+    res.json({
+      _id,
+      nome,
+      email,
+      session: {
+        originalMaxAge: cookie.originalMaxAge,
+        expires: cookie.expires,
+      },
+    });
   }
 }
 
