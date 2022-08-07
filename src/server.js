5c989dd5eb3a861dbfb60572d058c840ab1aa8fb
eduardop1p <el702985@gmail.com>
2022-08-07 14:07:44 -0300

    atz port server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen(process.env.SERVER_PORT, () => {
+app.listen(process.env.SERVER_PORT || 4000, () => {
   console.log(`Sevidor rodando em ${process.env.APP_URL}`);
 });
