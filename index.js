f930baa249fa2cec8868e56778e412a6c8813e15
eduardop1p <el702985@gmail.com>
2022-10-31 21:46:21 -0300

    server test no vercel

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Router } = require('express');
+
+const app = express();
+const router = Router();
+
+const routerGetHome = router.get('/', (req, res) => {
+  try {
+    res.json({ success: 'api na homer rodando' });
+  } catch {
+    res.status(500).json({
+      error: 'internal server erro',
+    });
+  }
+});
+const routerGetUser = router.get('/user', (req, res) => {
+  try {
+    res.json({ user: { name: 'Eduardo', idade: 20 } });
+  } catch {
+    res.status(500).json({
+      error: 'internal server erro',
+    });
+  }
+});
+
+app.use(routerGetHome);
+app.use(routerGetUser);
+app.use(express.json());
+
+app.listen(4000, () => console.log('sevidor rodando em http://localhost:4000'));
