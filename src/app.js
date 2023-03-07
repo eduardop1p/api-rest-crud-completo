4644f2ae07866579df410c0337fba3bbaf23c060
Eduardo Oliveira <el702985@gmail.com>
2023-03-07 17:16:56 -0300

    adcionando domínios autorizados para o frame-ancestors #4

--- src/app.js
+++ src/app.js
@@ -34,7 +34,12 @@ class App {
       helmet({
         crossOriginResourcePolicy: { policy: 'cross-origin' },
         contentSecurityPolicy: {
-          directives: { frameAncestors: ['*'] },
+          directives: {
+            frameAncestors: [
+              'http://127.0.0.1:5500/',
+              'https://eduardo-lavoura.vercel.app/',
+            ],
+          },
         },
       })
     );
